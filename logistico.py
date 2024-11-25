2be72338b0629b5a26495fb2ba2c51ad63f4deee
Mario Cervantes <42220642+cervanmd@users.noreply.github.com>
2024-11-25 14:51:03 -0300

    Update logistico.py

--- logistico.py
+++ logistico.py
@@ -34,4 +34,67 @@ y = df['STATUS'] #establecemos la variable "STATUS" como variable de interés
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42) #se realiza la partición prueba y entrenamiento, "test_size" hace referencia a la proporción de división, en este caso, mitad y mitad.
 #random_state hace referencia a la distribución aleatoria de estas particiones. Este asegur una aleatoriedad en la partición cada vez que se ejecute el código
 
+#importamos la función de regresión logística
+from sklearn.linear_model import LogisticRegression
+
+#definimos el clasificador
+clf = LogisticRegression(random_state=0).fit(X, y)
+
+
+logModel = LogisticRegression()
+
+#buscamos la mejor combinación de hiperparámetros que expliquen nuestro problema
+param_grid = [
+    {'penalty' : ['l1', 'l2', 'elasticnet', 'none'],
+    'C' : (0.001, 100, 20),
+    'solver' : ['lbfgs','newton-cg','liblinear','sag','saga'],
+    'max_iter' : [100, 1000,2500, 5000, 1000000]
+    }
+]
+
+#importamos la búsqueda de red
+from sklearn.model_selection import GridSearchCV
+
+clf = GridSearchCV(logModel, param_grid = param_grid, cv = 3, verbose=True, n_jobs=-1)
+
+best_clf = clf.fit(X,y)
+
+best_clf.best_estimator_
+
+
+print (f'Accuracy Train - : {best_clf.score(X_train,y_train):.3f}')
+print (f'Accuracy Test - : {best_clf.score(X_test,y_test):.3f}')
+
+
+#Obtiene los coeficiones que mejor explican al modelo en orden descendente
+
+coefficients = best_clf.best_estimator_.coef_
+feature_names = X.columns
+
+for i in range(len(feature_names)):
+  print(f"{feature_names[i]}: {coefficients[0][i]}")
+
+
+
+
+#imprimimos los pesos
+
+import matplotlib.pyplot as plt
+
+# Extrae las variables
+feature_names = X.columns
+coefficients = best_clf.best_estimator_.coef_[0]
+
+# Genera un gráfico de barras
+plt.figure(figsize=(10, 6))
+plt.bar(feature_names, coefficients)
+plt.xlabel("Features")
+plt.ylabel("Coefficient Weight")
+plt.title("Feature Weights in Logistic Regression Model")
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.show()
+
+
+#las variables que conformen este modelo pueden ser  usadas como entrada para un modelo con más potencia, como una máquina de soporte de vectores.
 
